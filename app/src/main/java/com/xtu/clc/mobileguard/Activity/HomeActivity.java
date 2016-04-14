747bd443ef69ebc67f4a544ff7710e5a1d5475c0
yclvergil <yclVergil@gmail.com>
2016-04-14 20:38:27 +0800

    splash界面，连接网络、检查更新

---
+++ app/src/main/java/com/xtu/clc/mobileguard/Activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.xtu.clc.mobileguard.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xtu.clc.mobileguard.R;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/xtu/clc/mobileguard/Activity/SplashActivity.java
+++ app/src/main/java/com/xtu/clc/mobileguard/Activity/SplashActivity.java
@@ -1,7 +1,15 @@
 package com.xtu.clc.mobileguard.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -9,6 +17,7 @@ import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.xtu.clc.mobileguard.R;
 import com.xtu.clc.mobileguard.bean.UrlBean;
@@ -21,21 +30,47 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+
 public class SplashActivity extends AppCompatActivity {
+    private static final int LOADMAIN = 1;
+    private static final int SHOW_UPDATE_DIALOG = 2;
     private RelativeLayout rl_root;
+    private int versionCode;//版本号
+    private String versionName;//版本名
+    private TextView tv_versionName;
+    private UrlBean bean;
+    private long startTimeMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //初始化视图
         initView();
+        //初始化版本信息
+        initData();
         //初始化动画
         initAnimation();
         //检查服务器版本
         checkVersion();
+    }
+
+    /**
+     * 初始化版本名
+     */
+    private void initData() {
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo(getPackageName(), 0);
+            versionCode = info.versionCode;
+            versionName = info.versionName;
+            tv_versionName.setText(versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -46,6 +81,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
+                    startTimeMillis = System.currentTimeMillis();
                     URL url = new URL("http://10.0.2.2:8087/guardversion.json");
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setReadTimeout(5000);//读取数据超时
@@ -63,8 +99,8 @@ public class SplashActivity extends AppCompatActivity {
 
                         }
                         //解析
-                        UrlBean bean = parseJson(jsonString);
-                        Log.d("SplashActivity", "jsonString:Version" + bean.getVersionCode());
+                        bean = parseJson(jsonString);
+                        isNewVersion(bean);
                         br.close();
                         conn.disconnect();
                     }
@@ -73,6 +109,80 @@ public class SplashActivity extends AppCompatActivity {
                 }
             }
         }).start();
+    }
+
+    /**
+     * 在UI线程Handler处理消息
+     */
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case LOADMAIN:
+                    loadMain();//进入主界面
+                    break;
+                case SHOW_UPDATE_DIALOG:
+                    showUpdateDialog();
+                    break;
+            }
+        }
+    };
+
+    /**
+     * 创建更新对话框
+     */
+    private void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提醒")
+                .setMessage("是否更新新版本？新版本具有如下特性：" + bean.getDesc())
+                .setPositiveButton("更新", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("SplashActivity", "更新APK");
+                        downloadNewApk();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        loadMain();
+                    }
+                });
+        builder.show();
+    }
+
+    /**
+     * 下载新版本Apk
+     */
+    private void downloadNewApk() {
+
+    }
+
+    /**
+     * 加载主界面
+     */
+    private void loadMain() {
+        Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * 检查是否有新版本
+     */
+    private void isNewVersion(UrlBean bean) {
+        int serverCode = bean.getVersionCode();//获取的服务器版本
+        long endTimeMillis = System.currentTimeMillis();
+        if (endTimeMillis - startTimeMillis < 3000) {
+            //设置休眠的时间保证至少休眠三秒
+            SystemClock.sleep(3000 - (endTimeMillis - startTimeMillis));
+        }
+        if (serverCode == versionCode) {
+            //发送进入主界面的消息
+            mHandler.sendEmptyMessage(LOADMAIN);
+        } else {
+            //有新版本，弹出更新对话框的消息
+            mHandler.sendEmptyMessage(SHOW_UPDATE_DIALOG);
+        }
     }
 
     /**
@@ -134,6 +244,6 @@ public class SplashActivity extends AppCompatActivity {
     private void initView() {
         setContentView(R.layout.activity_splash);
         rl_root = (RelativeLayout) findViewById(R.id.rl_splash_root);
-
+        tv_versionName = (TextView) findViewById(R.id.tv_splash_version_name);
     }
 }
